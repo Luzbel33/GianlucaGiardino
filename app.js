b1c5645575b54ae3c9e450792f0747db0a3a52e9
Luz Belle <gravitty99@gmail.com>
2025-08-13 20:53:19 -0300

    Renovado todo

--- app.js
+++ app.js
@@ -5,9 +5,13 @@
             this.classList.add("active-btn");
             document.querySelector(".active").classList.remove("active");
             document.getElementById(button.dataset.id).classList.add("active");
+			// Scroll al tope al cambiar de sección
+			window.scrollTo({ top: 0, behavior: 'smooth' });
         })
     });
+    /*
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
     })
+    */
 })();
